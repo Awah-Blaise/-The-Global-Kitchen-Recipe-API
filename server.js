072a66337b66fede0bb6cb442916cf7231b46207
Awah Blaise <blaiseawah22@gmail.com>
2026-05-18 14:00:33 +0100

    Removed env file

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+const recipeRoutes = require("./routes/recipeRoutes");
+
+const errorHandler = require("./middleware/errorHandler");
+const notFound = require("./middleware/notFound");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("The Global Kitchen API Running...");
+});
+
+app.use("/recipes", recipeRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
